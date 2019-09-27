60e609d719dac89749627986ea591607dc71ea9b
mkmdev <mkmdev18@gmail.com>
2019-09-27 12:49:18 +0200

    BUGFIX

--- src/app/components/home/news/news-list/news-item/add-news/add-news.component.ts
+++ src/app/components/home/news/news-list/news-item/add-news/add-news.component.ts
@@ -3,6 +3,7 @@ import {NgForm} from '@angular/forms';
 import {PostService} from '../../../../../../services/post.service';
 import {MemberService} from '../../../../../../services/member.service';
 import {faPlusCircle} from '@fortawesome/free-solid-svg-icons';
+import {Member} from '../../../../../../models/Member';
 
 @Component({
   selector: 'app-add-news',
@@ -14,12 +15,12 @@ export class AddNewsComponent implements OnInit {
   @ViewChild('f', {static: false}) newPostForm: NgForm;
   faPlus = faPlusCircle;
   defaultCategory = 'NEWS';
-  loggedInUsername = '';
+  loggedInUsername = 'test';
   isPublished = false;
   submitted = false;
   post = {
     id: 0,
-    author: null,
+    author: new Member(),
     title: '',
     isPublished: false,
     publishDate: null,
@@ -34,20 +35,22 @@ export class AddNewsComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.post.author = this.memberService.getDummyUser();
-    this.loggedInUsername = this.post.author.username;
-    console.log(this.loggedInUsername);
+    this.memberService.getDummyUser().subscribe(author => {
+      this.post.author = author;
+      this.loggedInUsername = this.post.author.username;
+      console.log(this.post.author.username);
+    });
   }
 
   onSubmit(f: NgForm) {
     console.log(this.newPostForm);
     this.submitted = true;
-    this.post.author = this.newPostForm.value.newPostData.author;
+    // this.post.author = this.newPostForm.value.newPostData.author;
     this.post.title = this.newPostForm.value.newPostData.title;
     this.post.isPublished = this.newPostForm.value.newPostData.isPublished;
     this.post.category = this.newPostForm.value.newPostData.category;
     this.post.postBody = this.newPostForm.value.newPostData.postBody;
-    this.postService.addNews(this.post);
+    this.postService.addNews(this.post).subscribe();
     this.newPostForm.reset();
   }
 }
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -20,7 +20,6 @@ export class PostService {
 
   constructor(
     private http: HttpClient) {
-
   }
 
   getNews(): Observable<Post[]> {
