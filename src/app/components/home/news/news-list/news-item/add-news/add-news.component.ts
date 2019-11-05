f24c17a498393973daffa654165f65883b23415e
mkmdev <mkmdev18@gmail.com>
2019-11-05 16:12:15 +0100

    fixing form initialiation after reset()

--- src/app/components/home/news/news-list/news-item/add-news/add-news.component.ts
+++ src/app/components/home/news/news-list/news-item/add-news/add-news.component.ts
@@ -34,20 +34,30 @@ export class AddNewsComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
     this.memberService.getDummyUser().subscribe(author => {
       this.post.author = author;
       this.loggedInUsername = this.post.author.username;
     });
+    this.post.category = this.defaultCategory;
+    console.log('Debug category' + this.post.category);
+    this.post.isPublished = false;
   }
 
   onSubmit(f: NgForm) {
-    console.log(this.newPostForm);
+    console.log(f);
     this.submitted = true;
     this.post.title = this.newPostForm.value.newPostData.title;
     this.post.isPublished = this.newPostForm.value.newPostData.isPublished;
     this.post.category = this.newPostForm.value.newPostData.category;
     this.post.postBody = this.newPostForm.value.newPostData.postBody;
-    this.postService.addNews(this.post).subscribe();
-    this.newPostForm.reset();
+    this.postService.addNews(this.post).subscribe(res => {
+      this.newPostForm.reset();
+      this.initForm();
+    });
+
   }
 }
