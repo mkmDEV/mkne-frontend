97ffd4932438f3ea8234319de4d1a8a48f182b37
mkmdev <mkmdev18@gmail.com>
2019-11-06 10:46:31 +0100

    finished angular-driven bootstrap module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,9 @@ import {NewsComponent} from './components/home/news/news.component';
 import {HomeComponent} from './components/home/home.component';
 import {NewsListComponent} from './components/home/news/news-list/news-list.component';
 import {NewsItemComponent} from './components/home/news/news-list/news-item/news-item.component';
-import {BsDropdownModule, ModalModule, PopoverModule, TooltipModule} from 'ngx-bootstrap';
+import {BsDropdownModule, CarouselModule, ModalModule, PopoverModule, TooltipModule} from 'ngx-bootstrap';
 import {TopNavBarComponent} from './components/home/top-nav-bar/top-nav-bar.component';
-import {AnimatedLogoComponent} from './components/home/animated-logo/animated-logo.component';
+import {AnimatedLogoComponent} from './components/home/top-nav-bar/animated-logo/animated-logo.component';
 import {AdvertsComponent} from './components/home/sidebar/adverts/adverts.component';
 import {SidebarComponent} from './components/home/sidebar/sidebar.component';
 import {AdvertComponent} from './components/home/sidebar/adverts/advert/advert.component';
@@ -37,15 +37,16 @@ import {FooterComponent} from './components/home/footer/footer.component';
     FooterComponent,
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    FontAwesomeModule,
     AppRoutingModule,
+    BrowserModule,
     BsDropdownModule.forRoot(),
-    TooltipModule.forRoot(),
-    ModalModule.forRoot(),
+    CarouselModule.forRoot(),
+    FontAwesomeModule,
     FormsModule,
-    PopoverModule
+    HttpClientModule,
+    ModalModule.forRoot(),
+    PopoverModule,
+    TooltipModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
