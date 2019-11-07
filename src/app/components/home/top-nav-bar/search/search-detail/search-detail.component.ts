93f66b885a3cb5e6fb21b7207d04251d322e0da8
mkmdev <mkmdev18@gmail.com>
2019-11-07 14:22:52 +0100

    Sketch up search details

--- src/app/components/home/top-nav-bar/search/search-detail/search-detail.component.ts
+++ src/app/components/home/top-nav-bar/search/search-detail/search-detail.component.ts
@@ -1,16 +1,28 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-search-detail',
   templateUrl: './search-detail.component.html',
   styleUrls: ['./search-detail.component.css']
 })
-export class SearchDetailComponent implements OnInit {
+export class SearchDetailComponent implements OnInit, OnDestroy {
+  private routeSub: any;
+  query: string;
 
-  constructor() {
+  constructor(
+    private route: ActivatedRoute
+  ) {
   }
 
   ngOnInit() {
+    this.routeSub = this.route.params.subscribe(params => {
+      this.query = params['q'];
+    });
+  }
+
+  ngOnDestroy() {
+    this.routeSub.unsubscribe();
   }
 
 }
