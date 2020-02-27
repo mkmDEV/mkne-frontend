16e4a2ff209cb4124ff7f3a2af51319e958be55a
mkmdev <mkmdev18@gmail.com>
2020-02-27 18:34:19 +0100

    + endpoints for auth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,11 @@
 export const environment = {
   production: true,
+  REGISTER_URL: 'http://localhost:8080/register',
+  LOGIN_URL: 'http://localhost:8080/login',
+  MEMBER_URL: 'http://localhost:8080/user/member',
+  LOGGED_IN_MEMBER_URL: 'http://localhost:8080/user/logged-in-member',
   POSTS_URL: 'http://localhost:8080/posts',
   NEWS_URL: 'http://localhost:8080/posts/news',
   WEATHER_API_KEY: '37bd7ebe43f9ccf2058d4f1a201d6b94',
-  WEATHER_API_URL: 'http://api.openweathermap.org/data/2.5/weather/',
+  WEATHER_API_URL: 'http://api.openweathermap.org/data/2.5/weather/'
 };
