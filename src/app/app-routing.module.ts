a25dd96d562c3e353b754756341e0fed47cfa17f
mkmdev <mkmdev18@gmail.com>
2022-10-01 00:59:19 +0200

    [FIX]: feature/ui     - replaced ngx bootstap with ng-bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { AddNewsComponent } from './components/home/news/news-list/news-item/add-news/add-news.component';
-import { SearchDetailComponent } from './components/home/top-nav-bar/search/search-detail/search-detail.component';
+import { SearchDetailComponent } from './components/home/header/search/search-detail/search-detail.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,34 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { NewsComponent } from './components/home/news/news.component';
-import { HomeComponent } from './components/home/home.component';
-import { NewsListComponent } from './components/home/news/news-list/news-list.component';
-import { NewsItemComponent } from './components/home/news/news-list/news-item/news-item.component';
-import { AlertModule } from 'ngx-bootstrap/alert';
+import { NewsComponent } from '@components/home/news/news.component';
+import { HomeComponent } from '@components/home/home.component';
+import { NewsListComponent } from '@components/home/news/news-list/news-list.component';
+import { NewsItemComponent } from '@components/home/news/news-list/news-item/news-item.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
-import { ModalModule } from 'ngx-bootstrap/modal';
-import { PopoverModule } from 'ngx-bootstrap/popover';
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
-import { TopNavBarComponent } from './components/home/top-nav-bar/top-nav-bar.component';
-import { AnimatedLogoComponent } from './components/home/top-nav-bar/animated-logo/animated-logo.component';
-import { AdvertsComponent } from './components/home/sidebar/adverts/adverts.component';
-import { SidebarComponent } from './components/home/sidebar/sidebar.component';
-import { AdvertComponent } from './components/home/sidebar/adverts/advert/advert.component';
+import { HeaderComponent } from '@components/home/header/header.component';
+import { AnimatedLogoComponent } from '@components/home/header/animated-logo/animated-logo.component';
+import { AdvertsComponent } from '@components/home/sidebar/adverts/adverts.component';
+import { SidebarComponent } from '@components/home/sidebar/sidebar.component';
+import { AdvertComponent } from '@components/home/sidebar/adverts/advert/advert.component';
+import {
+  NgbAlertModule as AlertModule,
+  NgbCarouselModule as CarouselModule,
+  NgbDropdownModule as BsDropdownModule,
+  NgbModalModule as ModalModule,
+  NgbModule,
+  NgbPopoverModule as PopoverModule,
+  NgbTooltipModule as TooltipModule,
+} from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { AddNewsComponent } from './components/home/news/news-list/news-item/add-news/add-news.component';
+import { AddNewsComponent } from '@components/home/news/news-list/news-item/add-news/add-news.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { CarouselComponent } from './components/home/carousel/carousel.component';
-import { FooterComponent } from './components/home/footer/footer.component';
-import { SearchComponent } from './components/home/top-nav-bar/search/search.component';
-import { SearchDetailComponent } from './components/home/top-nav-bar/search/search-detail/search-detail.component';
-import { WeatherWidgetComponent } from './components/home/sidebar/weather-widget/weather-widget.component';
-import { SearchItemComponent } from './components/home/top-nav-bar/search/search-detail/search-item/search-item.component';
-import { PostService } from './services/post.service';
-import { WeatherService } from './services/weather.service';
+import { CarouselComponent } from '@components/home/carousel/carousel.component';
+import { FooterComponent } from '@components/home/footer/footer.component';
+import { SearchComponent } from '@components/home/header/search/search.component';
+import { SearchDetailComponent } from '@components/home/header/search/search-detail/search-detail.component';
+import { WeatherWidgetComponent } from '@components/home/sidebar/weather-widget/weather-widget.component';
+import { SearchItemComponent } from '@components/home/header/search/search-detail/search-item/search-item.component';
+import { PostService } from '@services/post.service';
+import { WeatherService } from '@services/weather.service';
 
 @NgModule({
   declarations: [
@@ -48,22 +51,23 @@ import { WeatherService } from './services/weather.service';
     SearchDetailComponent,
     SearchItemComponent,
     SidebarComponent,
-    TopNavBarComponent,
+    HeaderComponent,
     WeatherWidgetComponent,
   ],
   imports: [
-    AlertModule.forRoot(),
+    AlertModule,
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot(),
-    CarouselModule.forRoot(),
+    BsDropdownModule,
+    CarouselModule,
     FontAwesomeModule,
     FormsModule,
     HttpClientModule,
-    ModalModule.forRoot(),
+    ModalModule,
     PopoverModule,
-    TooltipModule.forRoot(),
+    TooltipModule,
+    NgbModule,
   ],
   providers: [PostService, WeatherService],
   bootstrap: [AppComponent],
